084100ea5c90ab0c0ed29c996a2c585fd57783a6
lvliqi <lvliqi_d@163.com>
2024-03-18 16:32:05 +0800

    feat: up jest.config.js

--- jest.config.ts
+++ jest.config.ts
@@ -173,8 +173,9 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest'
+    // '^.+\\.(ts|tsx)?$': 'ts-jest',
+    // '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { isolatedModules: true }]
   }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
